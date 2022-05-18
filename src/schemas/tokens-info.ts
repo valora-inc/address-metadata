3377891d150f150a4a212718e781c31be9c44a0e
Jean Regisser <jean.regisser@gmail.com>
2022-05-18 12:37:09 +0200

    Ensure the pegTo value exists in the object

--- src/schemas/tokens-info.ts
+++ src/schemas/tokens-info.ts
@@ -10,7 +10,7 @@ const TokensInfoSchema = Joi.object().pattern(
     decimals: Joi.number().required(),
     symbol: Joi.string().required(),
     isCoreToken: Joi.boolean(),
-    pegTo: Joi.string(),
+    pegTo: Joi.valid(Joi.ref('/', { in: true })),
   }),
 )
 
