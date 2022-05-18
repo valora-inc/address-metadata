33f9cf052d795893321d142a974929e6b05aee5e
Jean Regisser <jean.regisser@gmail.com>
2022-05-18 15:03:41 +0200

    Host tokens in this repo

---
+++ src/assets.test.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import path from 'path'
+import { promisify } from 'util'
+const sizeOfImage = promisify(require('image-size'))
+
+const ASSETS_DIRECTORY = './assets/tokens'
+const REQUIRED_ASSET_HEIGHT = 256
+const REQUIRED_ASSET_WIDTH = 256
+const REQUIRED_ASSET_TYPE = 'png'
+
+describe('assets', () => {
+  describe('tokens', () => {
+    const assetPaths = fs
+      .readdirSync(ASSETS_DIRECTORY)
+      .map((assetFilename) => path.join(ASSETS_DIRECTORY, assetFilename))
+
+    // TODO: update assets so they can pass these requirements
+    xit.each(assetPaths)('%s is the required size', async (assetPath) => {
+      const { height, width } = await sizeOfImage(assetPath)
+      expect(height).toBe(REQUIRED_ASSET_HEIGHT)
+      expect(width).toBe(REQUIRED_ASSET_WIDTH)
+    })
+
+    it.each(assetPaths)('%s is the required type', async (assetPath) => {
+      const { type: assetType } = await sizeOfImage(assetPath)
+      expect(assetType).toBe(REQUIRED_ASSET_TYPE)
+    })
+  })
+})
