df352c4343b4b209f099ae920c7348be706a9cac
anthonydaoud <anthonydaoud01@gmail.com>
2023-01-09 17:05:00 -0800

    formatted

--- src/schemas/addresses-extra-info.ts
+++ src/schemas/addresses-extra-info.ts
@@ -28,7 +28,7 @@ const AddressesExtraInfoSchema = Joi.object().pattern(
     name: Joi.string().required(),
     isProviderAddress: Joi.boolean(),
     isCeloRewardSender: Joi.boolean(),
-    isFiatConnectProvider: Joi.boolean()
+    isFiatConnectProvider: Joi.boolean(),
   }),
 )
 
