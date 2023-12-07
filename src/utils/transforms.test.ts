valora-inc/address-metadata

commit 8d88b805e28e0527acc4d42a353080d6b191fcf3

feat: replace `isCoreToken` with `isFeeCurrency` and `canTransferWithComment` (#306)
### Motivation

In the future we may have tokens which are accepted as a fee currency,
but may not support
[transferWithComment](https://celoscan.io/address/0x765de816845861e75a25fca122bb6898b8b1282a#writeProxyContract#F15)
method in their smart contract (e.g.
[GoodDollar](https://celoscan.io/token/0x62b8b11039fcfe5ab0c56e502b1c372a3d2a9c7a?a=0xb6d4ab615915f2570f879f7aeb986e204e1e694a#writeProxyContract)).

Although currently it's not the case: all accepted fee currency tokens
can transfer with comment, we want to prepare for it making the property
set more granular.

We want to decouple `isCoreToken` property to two separate ones:
* `isFeeCurrency`
* `canTransferWithComment`

### Test plan

* Updated unit tests: token descriptions containing `isCoreToken`
property must have `isFeeCurrency` property
* `isCoreToken` is kept intact to ensure backwards compatibility

### Related issues

Related to RET-899

See also https://github.com/valora-inc/address-metadata/pull/273

src/utils/transforms.ts
@@ -10,7 +10,8 @@ export function transformCeloTokensForRTDB(
     celoTokensInfo.map((rawTokenInfo) => {
       const { address, isNative, bridge, name: rawName, ...rest } = rawTokenInfo
       const name = bridge ? `${rawName} (${bridge})` : rawName
-      return [address, { address, name, ...rest }]
+      const isCoreToken = rawTokenInfo.isFeeCurrency // for backwards compatibility. `isCoreToken` is deprecated
+      return [address, { address, name, ...rest, isCoreToken }]
     }),
   )
 }

src/utils/transforms.test.ts
@@ -25,4 +25,29 @@ describe('transforms', () => {
     expect(transformData[address1].name).toBe('Test Token 1')
     expect(transformData[address2].name).toBe('Test Token 2 (Test Bridge)')
   })
+
+  it('should add `isCoreToken` property if token has `isFeeCurrency` property for backwards compatibility', () => {
+    const address1 = '0x00400fcbf0816bebb94654259de7273f4a05c762'
+    const address2 = '0x452ef5a4bd00796e62e5e5758548e0da6e8ccdf3'
+    const mockTokensInfo = [
+      {
+        address: address1,
+        name: 'Test Token 1',
+        symbol: 'TT1',
+        decimals: 18,
+      },
+      {
+        address: address2,
+        name: 'Test Token 2',
+        symbol: 'TT2',
+        decimals: 18,
+        isFeeCurrency: true,
+      },
+    ]
+
+    const transformData = transformCeloTokensForRTDB(mockTokensInfo)
+
+    expect(transformData[address1].isCoreToken).toBeFalsy()
+    expect(transformData[address2].isCoreToken).toBeTruthy()
+  })
 })

src/index.test.ts
@@ -31,7 +31,8 @@ describe('index', () => {
         decimals: 18,
         imageUrl:
           'https://raw.githubusercontent.com/valora-inc/address-metadata/main/assets/tokens/CELO.png',
-        isCoreToken: true,
+        isFeeCurrency: true,
+        canTransferWithComment: true,
         name: 'Celo',
         symbol: 'CELO',
         isNative: true,

src/schemas.test.ts
@@ -53,6 +53,19 @@ describe('Schema validation', () => {
         )
         expect(validationResult.error).toBeDefined()
       })
+      it('requires tokens having `isCoreToken` property to have the `isFeeCurrency` property', () => {
+        const validationResult = validateWithSchema(
+          {
+            address: '0x471ece3750da237f93b8e339c536989b8978a438',
+            name: 'Fee currency token',
+            symbol: 'XYZ',
+            decimals: 18,
+            isCoreToken: true,
+          },
+          TokenInfoSchemaJSON,
+        )
+        expect(validationResult.error).toBeDefined()
+      })
     })
     describe('TokenInfoSchemaProcessed', () => {
       it('forbids native tokens to have networkIconUrl', () => {
@@ -120,7 +133,8 @@ describe('Schema validation', () => {
               decimals: 18,
               imageUrl:
                 'https://raw.githubusercontent.com/valora-inc/address-metadata/main/assets/tokens/CELO.png',
-              isCoreToken: true,
+              isFeeCurrency: true,
+              canTransferWithComment: true,
               name: 'Celo',
               symbol: 'CELO',
               isSwappable: true,
@@ -171,6 +185,11 @@ describe('Schema validation', () => {
         for (const pegToAddress of pegToAddresses) {
           expect(addresses.includes(pegToAddress)).toEqual(true)
         }
+      }
+    })
+    it('sets deprecated property `isCoreToken` equal to `isFeeCurrency`', () => {
+      for (const tokenInfo of tokensInfo) {
+        expect(tokenInfo.isCoreToken).toEqual(tokenInfo.isFeeCurrency)
       }
     })
   })

src/schemas/tokens-info.ts
@@ -42,6 +42,8 @@ const BaseTokenInfoSchema = Joi.object({
   decimals: Joi.number().required(),
   symbol: Joi.string().required(),
   isCoreToken: Joi.boolean(),
+  isFeeCurrency: Joi.boolean(),
+  canTransferWithComment: Joi.boolean(),
   isSupercharged: Joi.boolean(),
   pegTo: AddressSchema,
   // Forcing flag to be false if decimals is not 18
@@ -62,6 +64,8 @@ const BaseTokenInfoSchema = Joi.object({
   isCashInEligible: Joi.boolean(),
   isCashOutEligible: Joi.boolean(),
 })
+  // Ensure `isFeeCurrency` is present when `isCoreToken` (deprecated) is present.
+  .with('isCoreToken', 'isFeeCurrency')
 
 const ProcessedTokenInfoSchema = BaseTokenInfoSchema.concat(
   Joi.object({

src/tokens-info.ts
@@ -33,6 +33,7 @@ export function getTokensInfoByNetworkIds(networkIds: NetworkId[]): {
         networkId,
         tokenId,
         networkIconUrl: tokenInfo.isNative ? undefined : nativeImageUrl,
+        isCoreToken: tokenInfo.isFeeCurrency, // for backwards compatibility. `isCoreToken` is deprecated
       }
     }
   }

src/types.ts
@@ -38,6 +38,8 @@ export interface TokenInfoJSON {
   imageUrl?: string
   isNative?: boolean
   isCoreToken?: boolean
+  isFeeCurrency?: boolean
+  canTransferWithComment?: boolean
   pegTo?: string
   isSupercharged?: boolean
   bridge?: string

commit 8629ab17b4bfa1a39488e47fa9262789b67aa5c3

fix: fix undefined properties issue (#309)
Fixing an issue introduced in
https://github.com/valora-inc/address-metadata/pull/306: tokens not
having `isFeeCurrency` property programmatically received `isCoreToken:
undefined` property which [caused RTDB update to
fail](https://github.com/valora-inc/address-metadata/actions/runs/7114775672/job/19369519963).

### Test plan

Updated unit tests

### Related issues

Related to RET-899

src/utils/transforms.ts
@@ -11,7 +11,15 @@ export function transformCeloTokensForRTDB(
       const { address, isNative, bridge, name: rawName, ...rest } = rawTokenInfo
       const name = bridge ? `${rawName} (${bridge})` : rawName
       const isCoreToken = rawTokenInfo.isFeeCurrency // for backwards compatibility. `isCoreToken` is deprecated
-      return [address, { address, name, ...rest, isCoreToken }]
+      return [
+        address,
+        {
+          address,
+          name,
+          ...rest,
+          ...(isCoreToken !== undefined ? { isCoreToken } : null),
+        },
+      ]
     }),
   )
 }

src/utils/transforms.test.ts
@@ -50,4 +50,34 @@ describe('transforms', () => {
     expect(transformData[address1].isCoreToken).toBeFalsy()
     expect(transformData[address2].isCoreToken).toBeTruthy()
   })
+
+  it('should not have undefined properties', () => {
+    const address1 = '0x00400fcbf0816bebb94654259de7273f4a05c762'
+    const address2 = '0x452ef5a4bd00796e62e5e5758548e0da6e8ccdf3'
+    const mockTokensInfo = [
+      {
+        address: address1,
+        name: 'Test Token 1',
+        symbol: 'TT1',
+        decimals: 18,
+      },
+      {
+        address: address2,
+        name: 'Test Token 2',
+        symbol: 'TT2',
+        decimals: 18,
+        isFeeCurrency: true,
+      },
+    ]
+
+    const transformData = transformCeloTokensForRTDB(mockTokensInfo)
+
+    for (const address of Object.keys(transformData)) {
+      expect(
+        Object.values(transformData[address]).every(isNotUndefined),
+      ).toBeTruthy()
+    }
+  })
 })
+
+const isNotUndefined = (value: any) => value !== undefined