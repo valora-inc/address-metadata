f96a6becd39dd9e351e36cb61a8edaa891d48f8d
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2024-04-30 08:18:37 +1200

    chore: Switch polygon testnet from mumbai to amoy (#463)

--- src/types.ts
+++ src/types.ts
@@ -70,7 +70,7 @@ export enum NetworkId {
   ['op-mainnet'] = 'op-mainnet',
   ['op-sepolia'] = 'op-sepolia',
   ['polygon-pos-mainnet'] = 'polygon-pos-mainnet',
-  ['polygon-pos-mumbai'] = 'polygon-pos-mumbai',
+  ['polygon-pos-amoy'] = 'polygon-pos-amoy',
   ['base-mainnet'] = 'base-mainnet',
   ['base-sepolia'] = 'base-sepolia',
 }
